1b4dbd86ac0fd5c2db1f8fbd7668d62bb77e6788
EC2 Default User <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2020-05-22 04:07:32 +0000

    kadai_profile

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>News</title>
-    </head>
-    <body>
-        <h1>ニュース作成画面</h1>
-    </body>
-</html>
+@section('title', 'Myprofile')
+
+
+@section('content')
+    <h1>Myプロフィール</h1>
+@endsection
