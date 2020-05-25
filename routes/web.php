ca9e85c405f9ddb3ed3961db8b073664c2d52e0b
EC2 Default User <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2020-05-25 07:40:51 +0000

    php-12課題

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
 
 //追記：上記応用をグループ化しました。
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 Auth::routes();
 
