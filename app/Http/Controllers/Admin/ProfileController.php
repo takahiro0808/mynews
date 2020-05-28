c164584f08442bce5f56e2ac37ee3b394deb9743
EC2 Default User <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2020-05-28 11:05:10 +0000

    php-16課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,27 +15,68 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
-      // 以下を追記
-    // Varidationを行う
-    
+        // 以下を追記
+        // Varidationを行う
         $this->validate($request, Profile::$rules);
-        //dd($request);
         $profile = new Profile;
         $form = $request->all();
         unset($form['_token']);
         $profile->fill($form);
         $profile->save();
-        //dd($profile);
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    //以下を追記
+  public function index(Request $request)
+  {
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+      //検索されたら検索結果を取得する
+      $posts = News::where('title, $cond_title')->get();
+    } else {
+      //それ以外は全てのニュースを取得する
+      $posts = News::all();
+    }
+    return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  
+ //以下を追記
+    public function edit(Request $request)
     {  
-        return view('admin/profile/edit');
+        
+        $profile = Profile::find($request->id);
+        if (empty($profile)){
+            abort(404);
+    }
+        return view('admin/profile/edit',['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        
+        if(isset($profile_form['image'])) {
+            $path =$request->file('image')->store('public/image');
+            $profile->imege_path = basename($path);
+            unset($profile_form['imege']);
+        }   elseif (isset($request->remove)) {
+            $profile->image_path = null;
+            unset($profile_form['remove']);
+        }
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile');
+    }
+    
+    public function delete(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        $profile->delete();
+        return redirect('admin/profile');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create');
-     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
-     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
-     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+     Route::get('news', 'Admin\NewsController@index');
+     Route::get('news/edit', 'Admin\NewsController@edit');
+     Route::post('news/edit', 'Admin\NewsController@update'); 
+     Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 //「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
@@ -35,6 +35,8 @@ admin/profile/edit にアクセスしたら ProfileController の edit Action �
 Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
