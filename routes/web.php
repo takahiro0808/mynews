4b9340de4e2861d8f796644c8cd254bc8a286eae
EC2 Default User <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2020-05-28 03:21:47 +0000

    php-15

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
      Route::post('news/create', 'Admin\NewsController@create');
+     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
+     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 //「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
